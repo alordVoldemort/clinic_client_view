5c9e812e083aeb7fc45eccf776f05781db6aa4f5
Pooja Masne <poojamasne44@gmail.com>
2025-12-19 13:00:31 +0530

    home page images add

--- src/pages/Home/ExpertDoctorsSection.jsx
+++ src/pages/Home/ExpertDoctorsSection.jsx
@@ -1,24 +1,25 @@
 import { Box, Container, Grid, Typography } from "@mui/material";
 import DoctorCard from "./DoctorCard";
+import DrNitin from "../../assets/Expert_Teams/Dr. Nitin Darda.png";
+import DrYogita from "../../assets/Expert_Teams/Dr. Yogita Darda.png";
+import DrTanmay from "../../assets/Expert_Teams/Dr. Tanmay Darda.png";
+
 
 const doctors = [
   {
     name: "Dr. Nitin Darda",
     specialty: "Homeopathy Consultant",
-    image:
-      "https://images.unsplash.com/photo-1612349317150-e413f6a5b16d?w=400&h=500&fit=crop",
+    image: DrNitin,
   },
   {
     name: "Dr. Yogita Darda",
     specialty: "Homeopathy Consultant",
-    image:
-      "https://images.unsplash.com/photo-1559839734-2b71ea197ec2?w=400&h=500&fit=crop",
+    image: DrYogita,
   },
   {
     name: "Dr. Tanmay Darda",
     specialty: "Homeopathy Consultant",
-    image:
-      "https://images.unsplash.com/photo-1582750433449-648ed127bb54?w=400&h=500&fit=crop",
+    image: DrTanmay
   },
 ];
 
